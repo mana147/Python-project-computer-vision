f6a659ca07a40e4a88278ee626894924ecca49b1
mana147 <phamhieu078@gmail.com>
2019-12-20 22:08:19 +0700

    update : test numpy

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Python-project-computer-vision
 Python-Realtime-Action-Recognition
+
+pip install -r requirements.txt
+
+Setup environment python
+- install python3 
+- install pip3 for python3
+- install pip install virtualenv
+
+- pip list :
+    - 
+
+
+pip install https://developer.download.nvidia.com/compute/redist/jp/v42/tensorflow-gpu/tensorflow_gpu-1.14.0+nv19.10-cp36-cp36m-linux_aarch64.whl
---
+++ python_test/test_12_numpy.py
@@ -0,0 +1,39 @@
+from builtins import len
+import matplotlib.pyplot as plt
+import numpy as np
+import collections
+
+
+def void(name):
+    if not name:
+        print("--------------------------------")
+    else:
+        print("---------" + name + "-----------")
+
+
+# ---------------------------------------------------------------------
+
+vector_a = np.array(['a', 100, 'b', 'c', 11, 90, 'str', 10])
+vector_b = np.array([['v', 0,  1, 99, 6], ['b', 2,  5, 00, 7]])
+A = np.array([
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12]
+])
+# ---------------------------------------------------------------------
+
+void("data A")
+print(A)
+
+print("data:", A[1:1])
+collections.Counter(A)
+print("kieu du lieu", A.dtype)  # kieu du lieu
+print("so hang , so cot ", A.shape)  # so hang va cot
+print("chuyen vi matran", A.T)  # chuyen vi ma tran
+
+print("ravel", A.ravel())
+# ---------------------------------------------------------------------
+
+void("re_shape")
+re_A = A.reshape(1, 12)
+print(re_A)
